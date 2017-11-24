9e95e3be09446aee509d4e0496ce1473bed46c13
Christine Murad <christine.murad@mail.utoronto.ca>
2017-11-24 10:15:16 -0500

    Rewrote scripts

---
+++ Assets/Scripts/ControllerScript.cs
@@ -0,0 +1,65 @@
+﻿/* Adjusted from source: https://www.raywenderlich.com/149239/htc-vive-tutorial-unity */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerScript : MonoBehaviour {
+
+	private SteamVR_TrackedObject trackedObj;
+	public GameObject collidingObject; 
+//	private Vector3 lastPosition; // the controller's last position
+//	private Vector3 nearestVertex;
+	private bool holding;
+	Vector3 controllerPosition;
+
+
+	public SteamVR_Controller.Device Controller
+	{
+		get { return SteamVR_Controller.Input((int)trackedObj.index); }
+	}
+
+	void Awake()
+	{
+		trackedObj = GetComponent<SteamVR_TrackedObject>();
+//		lastPosition = Vector3.zero;
+	}
+
+	void Update() {
+		controllerPosition = transform.position;
+	}
+
+	/* -------- Trigger Functions -------- */
+		
+	public void OnTriggerEnter(Collider other)
+	{
+        if (holding == false)
+        {
+            SetCollidingObject(other);
+        }
+	}
+		
+	public void OnTriggerExit(Collider other)
+	{
+		if (!collidingObject)
+		{
+			return;
+		}
+
+        if (holding == false)
+        {
+            collidingObject = null;
+        }
+	}
+
+	/* -------- Helpers --------- */
+
+	private void SetCollidingObject(Collider col)
+	{
+        if (collidingObject || !col.GetComponent<Rigidbody>())
+		{
+			return;
+		}
+		collidingObject = col.gameObject; 
+	}
+}
--- Assets/Scripts/MeshDeformation.cs
+++ Assets/Scripts/MeshDeformation.cs
@@ -6,6 +6,8 @@ public class MeshDeformation : MonoBehaviour {
 
 	public static Mesh mesh;
 	private Vector3[] meshVertices;
+	public GameObject controllerOne;
+	public GameObject controllerTwo;
 
 
 	// Use this for initialization
@@ -14,27 +16,55 @@ public class MeshDeformation : MonoBehaviour {
 		meshVertices = mesh.vertices;
 
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-        // play/pause, reset keyboard listeners
-	}
+		ControllerScript controlOneScript = controllerOne.GetComponent<ControllerScript>();
+		ControllerScript controlTwoScript = controllerTwo.GetComponent<ControllerScript>();
 
-	public void Deform(Vector3 oldVertex, int index, Vector3 between, Vector3 newVertex) {
+		SteamVR_Controller.Device controlOne = controlOneScript.Controller;
+		SteamVR_Controller.Device controlTwo = controlTwoScript.Controller;
 
 		Vector3[] vertices = mesh.vertices;
-		Vector3[] newVertices = getNewVertices (oldVertex, index, between, newVertex);
+		Vector3[] newVertices = mesh.vertices;
+
+		// if pressing controller one button and controller one is colliding with this object
+		if (controlOne.GetHairTrigger () && controlOneScript.collidingObject == gameObject) {
+			// get controller one position and nearest index and do the deformation 
+			Vector3 controllerOnePos = controllerOne.transform.position;
+			int controllerOneIndex;
+			Vector3 controllerOneNearest;
+
+			NearestVertexTo (controllerOnePos, out controllerOneIndex, out controllerOneNearest);
+			newVertices = getNewVertices (controllerOnePos, controllerOneIndex, controllerOneNearest);
+		}
+
+		// if pressing controller two button and controller one is colliding with this object
+		if (controlTwo.GetHairTrigger () && controlTwoScript.collidingObject == gameObject) {
+			// get controller two position and nearest index and do the deformation 
+			Vector3 controllerTwoPos = controllerTwo.transform.position;
+			int controllerTwoIndex;
+			Vector3 controllerTwoNearest;
+
+			NearestVertexTo (controllerTwoPos, out controllerTwoIndex, out controllerTwoNearest);
+			newVertices = getNewVertices (controllerTwoPos, controllerTwoIndex, controllerTwoNearest);
+		}
+
+		// re-render mesh based on new vertices
 		int i = 0;
+
 		while (i < vertices.Length) {
 			vertices[i] = newVertices[i];
 			i++;
 		}
+
 		mesh.vertices = vertices;
 		mesh.RecalculateBounds();
-        meshVertices = vertices;
+		meshVertices = vertices;
 	}
-
-	private Vector3[] getNewVertices(Vector3 oldVertex, int index, Vector3 between, Vector3 newVertex) {
+		
+	private Vector3[] getNewVertices(Vector3 controllerPos, int controllerPosIndex, Vector3 controllerNearest) {
+		
 		Vector3[] originalVertices = meshVertices; // original vertices of this mesh
 
 		// Get and return the new vertices
@@ -43,13 +73,38 @@ public class MeshDeformation : MonoBehaviour {
 		Vector3[] newVertices = new Vector3[originalVertices.Length];
 
 		for (int i = 0; i < originalVertices.Length; i++) {
-            newVertices[i] = originalVertices[i] + between;
-            // newVertices[i] = originalVertices[i];
+//            newVertices[i] = originalVertices[i] + between;
 		}
 
-        // newVertices[index] = newVertex;
-
 		return newVertices;
+	}
+
+	public void NearestVertexTo(Vector3 point, out int index, out Vector3 nearest)
+	{
+
+		// Debug.Log("Getting Nearest Vertex");
+		// convert point to local space
+		point = transform.InverseTransformPoint(point);
+
+		Mesh mesh = GetComponent<MeshFilter>().mesh;
+		float minDistanceSqr = Mathf.Infinity;
+		Vector3 nearestVertex = Vector3.zero;
+		index = 0;
+		// scan all vertices to find nearest
+		for (int i = 0; i < mesh.vertices.Length; i++)
+		{
+			Vector3 vertex = mesh.vertices[i];
+			Vector3 diff = point-vertex;
+			float distSqr = diff.sqrMagnitude;
+			if (distSqr < minDistanceSqr)
+			{
+				minDistanceSqr = distSqr;
+				nearestVertex = vertex;
+				index = i;
+			}
+		}
+		// convert nearest vertex back to world space
+		nearest = transform.TransformPoint(nearestVertex);
 	}
 
 }
