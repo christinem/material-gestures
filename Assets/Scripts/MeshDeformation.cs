e4b9e559aafd695b9d3f4cc0441c6092f0c017cd
Christine Murad <murad.christine@gmail.com>
2017-11-25 19:18:40 -0500

    Lawson (using christine's git): Refactored and now can interact with two (or more) objects.

--- Assets/Scripts/MeshDeformation.cs
+++ Assets/Scripts/MeshDeformation.cs
@@ -3,145 +3,145 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MeshDeformation : MonoBehaviour {
+    // My Parameters
+    double minSelectionDist = 0.15; // Chosen through experiment
 
-	public static Mesh mesh;
+    // Script parameters (set by Unity)
+    public GameObject controllerOne;
+    public GameObject controllerTwo;
+
+    // Controller state to simplify controller handling
+    private struct ControllerState
+    {
+        public int id; // Integer id to identify the controller
+        public bool isGrabbing; // Is the trigger currently depressed and was it close enough to a vert when it was pulled? 
+        public int grabbedVertIndex; // If isGrabbing, then is index to the grabbed vertex
+        public Vector3 pos; // Controller position 
+
+        public GameObject gameObject;
+        public ControllerScript script;
+        public SteamVR_Controller.Device device;
+    }
+
+    private ControllerState[] controllerStates;
+
+    public Mesh mesh;
 	private Vector3[] meshVertices;
-	public GameObject controllerOne;
-	public GameObject controllerTwo;
-
-    bool controllerOneCloseEnough = false;
-    bool controllerTwoCloseEnough = false;
-
 
     // Use this for initialization
     void Start () {
-		mesh = ((MeshFilter)gameObject.GetComponent("MeshFilter")).mesh;
-		meshVertices = mesh.vertices;
+        // Put the Unity controllers into an array to simplify the rest of the logic
+        // TODO put this into a proper class constructor
+        controllerStates = new ControllerState[2];
+        controllerStates[0].gameObject = controllerOne;
+        controllerStates[0].id = 0;
+        controllerStates[0].isGrabbing = false;
+        controllerStates[0].grabbedVertIndex = -1;
+        controllerStates[1].gameObject = controllerTwo;
+        controllerStates[1].id = 1;
+        controllerStates[1].isGrabbing = false;
+        controllerStates[1].grabbedVertIndex = -1;
 
+        // Get the mesh this script has been assigned to.
+        mesh = ((MeshFilter)gameObject.GetComponent("MeshFilter")).mesh;
+		meshVertices = mesh.vertices;
 	}
+
+    void updateControllers()
+    {
+        for(int i = 0; i < controllerStates.Length; i++)
+        {
+            ControllerScript script = controllerStates[i].gameObject.GetComponent<ControllerScript>();
+            SteamVR_Controller.Device device = script.Controller;
+            Vector3 pos = controllerStates[i].gameObject.transform.position;
+            int grabbedVertIndex = controllerStates[i].grabbedVertIndex;
+            bool isGrabbing = controllerStates[i].isGrabbing;
+
+            // Counterintuitively.. getHairTriggerDown() only returns true on the first frame that the trigger is pulled.
+            // This is good though because that's actually what we want.
+            if (device.GetHairTriggerDown())
+            {
+                Debug.Log(mesh.vertices.Length);
+                int nearestVertIndex = nearestVertexTo(pos);
+                Vector3 nearestVertPos = transform.TransformPoint(mesh.vertices[nearestVertIndex]);
+                double dist = (pos - nearestVertPos).magnitude;
+
+                if(dist < minSelectionDist)
+                {
+                    Debug.Log("Grabbed vert " + nearestVertIndex);
+                    isGrabbing = true;
+                    grabbedVertIndex = nearestVertIndex;
+                }
+            }
+
+            if (device.GetHairTriggerUp())
+            {
+                isGrabbing = false;
+            }
+
+            controllerStates[i].isGrabbing = isGrabbing;
+            controllerStates[i].pos = pos;
+            controllerStates[i].grabbedVertIndex = grabbedVertIndex;
+            controllerStates[i].script = script;
+            controllerStates[i].device = device;
+        }
+    }
 
 	// Update is called once per frame
 	void Update () {
-		ControllerScript controlOneScript = controllerOne.GetComponent<ControllerScript>();
-		ControllerScript controlTwoScript = controllerTwo.GetComponent<ControllerScript>();
-
-		SteamVR_Controller.Device controlOne = controlOneScript.Controller;
-		SteamVR_Controller.Device controlTwo = controlTwoScript.Controller;
-
-        Vector3 controllerOnePos = controllerOne.transform.position;
-        int controllerOneIndex = 0;
-        Vector3 controllerOneNearest = Vector3.zero;
-
-        Vector3 controllerTwoPos = controllerTwo.transform.position;
-        int controllerTwoIndex = 0;
-        Vector3 controllerTwoNearest = Vector3.zero;
-
-        Vector3[] newVertices = mesh.vertices;
-
-		if (controlOne.GetHairTriggerDown()) {
-      		NearestVertexTo (controllerOnePos, out controllerOneIndex, out controllerOneCloseEnough, out controllerOneNearest);
-        }
-
-		if (controlTwo.GetHairTriggerDown()) {
-			NearestVertexTo (controllerTwoPos, out controllerTwoIndex, out controllerTwoCloseEnough, out controllerTwoNearest);
-		}
-
-       if (controlOne.GetHairTrigger() || controlTwo.GetHairTrigger())
-       //if (controllerOneCloseEnough || controllerTwoCloseEnough)
-        {
-            newVertices = getNewVertices(controllerOnePos, controllerOneNearest, controllerOneIndex, controllerOneCloseEnough,
-                                         controllerTwoPos, controllerTwoNearest, controllerTwoIndex, controllerTwoCloseEnough);
-            ReRender(newVertices);
-        }
-        
+        updateControllers();
+        updateVertices(controllerStates);
     }
 
-    private void ReRender(Vector3[] newVertices)
+    private void updateVertices(ControllerState[] controllerStates)
     {
-        Vector3[] vertices = mesh.vertices;
+        // We need to know whether the user is 'grabbing', which vertex the user is grabbing, and where the user's controller is located.
+        //Vector3[] originalVertices = meshVertices; // original vertices of this mesh
 
-        int i = 0;
-
-        while (i < vertices.Length)
+        foreach (ControllerState state in controllerStates)
         {
-            vertices[i] = newVertices[i];
-            i++;
-        }
+            if (state.isGrabbing)
+            {
+                Vector3[] origVerts = mesh.vertices;
 
-        mesh.vertices = vertices;
-        mesh.RecalculateBounds();
-        meshVertices = vertices;
+                Vector3 meshPoint = transform.TransformPoint(mesh.vertices[state.grabbedVertIndex]);
+                Vector3 offset = state.pos - meshPoint;
+ 
+                for (int i = 0; i < mesh.vertices.Length; i++)
+                {
+                    origVerts[i] += offset;
+                }
+
+                mesh.vertices = origVerts; // This is necessary to trigger an update for some reason >_<
+                mesh.RecalculateBounds(); // Todo Necessary?
+                return; // Just stop after first controller
+            }
+        }
     }
-		
-	private Vector3[] getNewVertices(Vector3 controllerOnePos, Vector3 controllerOneNearest, int controllerOneNearestIndex, 
-        bool controllerOneTriggered, Vector3 controllerTwoPos, Vector3 controllerTwoNearest, int controllerTwoNearestIndex, 
-        bool controllerTwoTriggered) {
-		
-		Vector3[] originalVertices = meshVertices; // original vertices of this mesh
 
-        // Get and return the new vertices
-
-        // Temporary testing code
-        Vector3[] newVertices = meshVertices;
-
-        if (controllerOneTriggered) {
-            Debug.Log("Controller 1");
-            for (int i = 0; i < originalVertices.Length; i++)
-            {
-                Vector3 between = controllerOnePos - controllerOneNearest;
-                newVertices[i] = originalVertices[i] + between;
-            }
-        }
-
-        if (controllerTwoTriggered)
-        {
-            Debug.Log("Controller 2");
-            for (int i = 0; i < originalVertices.Length; i++)
-            {
-                Vector3 between = controllerTwoPos - controllerTwoNearest;
-                newVertices[i] = originalVertices[i] + between;
-            }
-        }
-
-        return newVertices;
-	}
-
-	public void NearestVertexTo(Vector3 point, out int index, out bool isCloseEnough, out Vector3 nearest)
+	public int nearestVertexTo(Vector3 point)
 	{
-
-		// Debug.Log("Getting Nearest Vertex");
+        // Returns the index of the closest vert to point.
 		// convert point to local space
-		point = transform.InverseTransformPoint(point);
+		Vector3 localPoint = transform.InverseTransformPoint(point);
 
 		Mesh mesh = GetComponent<MeshFilter>().mesh;
 		float minDistanceSqr = Mathf.Infinity;
-		Vector3 nearestVertex = Vector3.zero;
-		index = 0;
-
-        Debug.Log(mesh.vertices);
+		int nearestVertIndex = 0;
+        
 		// scan all vertices to find nearest
 		for (int i = 0; i < mesh.vertices.Length; i++)
 		{
-			Vector3 vertex = mesh.vertices[i];
-			Vector3 diff = point-vertex;
+			Vector3 diff = localPoint - mesh.vertices[i];
 			float distSqr = diff.sqrMagnitude;
 			if (distSqr < minDistanceSqr)
 			{
 				minDistanceSqr = distSqr;
-				nearestVertex = vertex;
-				index = i;
+                nearestVertIndex = i;
 			}
 		}
 
-        if (minDistanceSqr < 3)
-        {
-            isCloseEnough = true;
-        } else
-        {
-            isCloseEnough = false;
-        }
-        // convert nearest vertex back to world space
-        nearest = transform.TransformPoint(nearestVertex);
+        return nearestVertIndex;
 	}
 
 }
